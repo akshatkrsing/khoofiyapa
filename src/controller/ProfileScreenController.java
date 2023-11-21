027489eaa778a79b77d1eac968711bc51e2d5c5a
Akshat <akshatkrsing@gmail.com>
2023-11-21 23:54:18 +0530

    Ignored out/

--- src/controller/ProfileScreenController.java
+++ src/controller/ProfileScreenController.java
@@ -383,6 +383,9 @@ public class ProfileScreenController implements Initializable {
             }
         }
     }
+    public void clearFileIconImageViews(){
+        currentFileIconImageView.getImage().
+    }
     public void encryptFolder(){
 
     }
